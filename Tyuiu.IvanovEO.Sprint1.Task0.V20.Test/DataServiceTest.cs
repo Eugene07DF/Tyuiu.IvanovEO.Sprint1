bf87fd4382cc505cd61f819c5f2f7a9ead8b72f8
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-09-27 19:20:57 +0500

    Task1

---
+++ Tyuiu.IvanovEO.Sprint1.Task0.V20.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.IvanovEO.Sprint1.Task1.V20.Lib;
+namespace Tyuiu.IvanovEO.Sprint1.Task1.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 1.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(10.5, res);
+        }
+    }
+}
