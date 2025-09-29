ba23d390ab797007c80c8208b2bb18e7234e5604
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-09-30 00:34:39 +0500

    Task5

---
+++ Tyuiu.IvanovEO.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.IvanovEO.Sprint1.Task5.V6.Lib;
+namespace Tyuiu.IvanovEO.Sprint1.Task5.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var solution = new DataService();
+            int input = 1;
+            int expected = 1;
+            int actual = solution.Calculate(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Tyuiu.IvanovEO.Sprint1.Task5.V6/Program.cs
@@ -0,0 +1,5 @@
+﻿using Tyuiu.IvanovEO.Sprint1.Task5.V6.Lib;
+Console.WriteLine("Введи n");
+int n = Convert.ToInt32(Console.ReadLine());
+DataService ds  = new DataService();
+Console.WriteLine(ds.Calculate(n));
