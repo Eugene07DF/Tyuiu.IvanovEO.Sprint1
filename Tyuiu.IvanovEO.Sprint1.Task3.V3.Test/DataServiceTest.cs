7ed8273b1613e9b323cb7e41037d2829537269a8
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-09-29 23:35:08 +0500

    Task3

---
+++ Tyuiu.IvanovEO.Sprint1.Task3.V3.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.WebSockets;
+using Tyuiu.IvanovEO.Sprint1.Task3.V3.Lib;
+namespace Tyuiu.IvanovEO.Sprint1.Task3.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValid()
+        {
+            DataService ds= new DataService();
+            double x = 2;
+            double y = 3;
+            double z = 4;
+            var res = ds.ParallelepipedVolume(x, y, z);
+            Assert.AreEqual(24, res);
+        }
+    }
+}
---
+++ Tyuiu.IvanovEO.Sprint1.Task3.V3/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.IvanovEO.Sprint1.Task3.V3.Lib;
+Console.WriteLine("Введите длинну");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите ширину");
+double y = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите высоту");
+double z = Convert.ToDouble(Console.ReadLine());
+DataService ds = new DataService();
+Console.WriteLine(Math.Round(ds.ParallelepipedVolume(x,y,z), 3));
