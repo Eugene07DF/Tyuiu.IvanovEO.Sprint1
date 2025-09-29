8a20e3870b4cb3fb53c79a9925cb27e0df9a1b50
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-09-30 00:16:12 +0500

    Task4

---
+++ Tyuiu.IvanovEO.Sprint1.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.IvanovEO.Sprint1.Task4.V5.Lib;
+namespace Tyuiu.IvanovEO.Sprint1.Task4.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValid()
+        {
+            DataService dataService = new DataService();
+            double x = 1;
+            double y = 1;
+            var res = dataService.Calculate(x, y);
+            Assert.AreEqual(0.5 , res);
+
+        }
+    }
+}
---
+++ Tyuiu.IvanovEO.Sprint1.Task4.V5/Program.cs
@@ -0,0 +1,4 @@
+﻿using Tyuiu.IvanovEO.Sprint1.Task4.V5.Lib;
+DataService ds = new DataService();
+double y, x;
+Console.WriteLine(ds.Calculate( x = Convert.ToDouble(Console.ReadLine()) , y = Convert.ToDouble(Console.ReadLine())));
